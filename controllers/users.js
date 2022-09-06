cc7be3f32b15e038a2c0a5876366030486ae3811
Celia Kelly <celiackelly@gmail.com>
2022-09-05 21:52:54 -0400

    Pass req.user to dashboard.ejs

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,6 @@ const User = require('../models/User')
 
 module.exports = {
     getDashboard: async (req, res) => {
-        res.render('dashboard.ejs', { title: 'Dashboard' })
+        res.render('dashboard', { title: 'Dashboard', user: req.user})
     }
 }
