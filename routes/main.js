03ce649928cbd1667f8bac223019ff5a0688a944
Celia Kelly <celiackelly@gmail.com>
2022-09-07 07:22:27 -0400

    Add Google auth- not working yet

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
+const passport = require('passport')
 const { ensureAuth } = require('../middleware/auth')
 const { ensureGuest } = require('../middleware/auth')
 
@@ -17,11 +18,28 @@ router.get('/login', ensureGuest, authController.getLogin)
 //POST to /sign-up to create an account
 router.post('/signup', authController.postSignup)
 
+//how to SIGN UP with Google? 
+
 //POST to /login to login (authenticate) a user
 router.post('/login', authController.postLogin)
 
+// @desc    Auth with Google
+// @route   GET /auth/google
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard')
+  }
+)
+
+//Change back from GET to DELETE
 //Handle DELETE requests to the /logout route - to logout/deauthenticate a use
 //Custom middleware in server.js handles overriding the link's GET method
-router.delete('/logout', authController.logout)
+router.get('/logout', authController.logout)
 
 module.exports = router
