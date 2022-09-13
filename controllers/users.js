d2e9404f9f890a46960b41600a8950b3e52a2247
Celia Kelly <celiackelly@gmail.com>
2022-09-13 19:17:37 -0400

    Fix dashboard course display

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const Course = require('../models/Course')
 module.exports = {
     getDashboard: async (req, res) => {
         try {
-            const courses = await Course.find({user: req.user})
+            const courses = await Course.find({userId: req.user.id})
             res.render('dashboard', { title: 'Dashboard', user: req.user, courses})
         } catch(err) {
             console.log(err)
