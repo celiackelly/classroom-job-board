cbf1423702e92a7f35654bac530226c863cdedb0
Celia Kelly <celiackelly@gmail.com>
2022-09-24 22:22:37 -0400

    Add randomize btn DOM manipulation

--- public/js/coursePageFunctions.js
+++ public/js/coursePageFunctions.js
@@ -1,3 +1,88 @@
+const assignNewJobsBtn = document.querySelector('#assign-new-jobs-btn')
+const randomizeBtn = document.querySelector('#randomize-btn')
+const saveBtn = document.querySelector('#save-btn')
+
+assignNewJobsBtn.addEventListener('click', () => {
+    randomizeBtn.classList.remove('hidden')
+    saveBtn.classList.remove('hidden')
+    assignNewJobsBtn.classList.add('hidden')
+})
+
+//Fisher-Yates Shuffle
+function shuffle(array) {   
+    let m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+}
+
+randomizeBtn.addEventListener('click', () => {
+    // make sure you're dealing with extra jobs and extra students...
+    const studentsInRoster = Array.from(document.querySelectorAll('[data-listname="roster"]')).map(el => {
+        return {
+            id: el.dataset.id, 
+            firstName: el.dataset.firstName,
+            lastName: el.dataset.lastName
+        }
+    })
+    console.log('original', studentsInRoster)
+    const jobList = Array.from(document.querySelectorAll('[data-listname="joblist"]')).map(el => {
+        return {
+            id: el.dataset.id, 
+            title: el.dataset.title
+        }
+    })
+    const shuffledStudents = shuffle(studentsInRoster.slice())
+    console.log('shuffled', shuffledStudents)
+
+    //use while loop to append students while there are still open jobs, and then add the rest to the unassigned students container? 
+    let studentSpans = shuffledStudents.map(student => {
+        let span = document.createElement('span')
+        span.dataset.id = student.id
+        span.dataset.firstName = student.firstName
+        span.dataset.lastName = student.lastName
+        span.textContent = `${student.firstName} ${student.lastName}`
+        return span
+    })
+
+    const tdCells = document.querySelectorAll('td')
+    tdCells.forEach((cell, i) => {
+        while (cell.firstChild) {
+            cell.removeChild(cell.firstChild)
+        }
+        if (studentSpans.length) {
+            cell.appendChild(studentSpans.shift())
+        }
+    })
+
+    const unassignedStudentsContainer = document.querySelector('.unassigned-students-container')
+    const unassignedStudentSpans = Array.from(unassignedStudentsContainer.querySelectorAll('span'))
+    unassignedStudentSpans.forEach(span => unassignedStudentsContainer.removeChild(span))
+
+    if (studentSpans.length) {
+        studentSpans.forEach(span => {
+            span.classList.add('rounded')
+            unassignedStudentsContainer.appendChild(span)
+        })
+    }
+})
+
+saveBtn.addEventListener('click', () => {
+   
+    //save to database 
+    //re-render the page...so no need to toggle button .hidden class
+})
+
+
 const addStudentRowBtn = document.querySelector('#add-student-row-btn')
 addStudentRowBtn.addEventListener('click', addStudentRow)
 //In the 'Add Students' modal, add another row of student name inputs when the plus icon is clicked
