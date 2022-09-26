c97aeffae04b4b46ac46d703f6be4a5763e63034
Celia Kelly <celiackelly@gmail.com>
2022-09-26 06:35:25 -0400

    Modify drag for horizontal orientation

--- public/js/coursePageFunctions.js
+++ public/js/coursePageFunctions.js
@@ -19,40 +19,40 @@ function enableDrag() {
     draggables.forEach(draggable => {
         draggable.setAttribute('draggable', true)
         draggable.addEventListener('dragstart', () => {
-          draggable.classList.add('dragging')
+            draggable.classList.add('dragging')
         })
       
         draggable.addEventListener('dragend', () => {
-          draggable.classList.remove('dragging')
+            draggable.classList.remove('dragging')
         })
       })
 
-      targets.forEach(target => {
+    targets.forEach(target => {
         target.addEventListener('dragover', e => {
-          e.preventDefault()
-          const afterElement = getDragAfterElement(target, e.clientY)
-          const draggable = document.querySelector('.dragging')
-          if (afterElement == null) {
-            target.appendChild(draggable)
-          } else {
-            target.insertBefore(draggable, afterElement)
-          }
+            e.preventDefault()
+            const afterElement = getDragAfterElement(target, e.clientX)
+            const draggable = document.querySelector('.dragging')
+            if (afterElement == null) {
+                target.appendChild(draggable)
+            } else {
+                target.insertBefore(draggable, afterElement)
+            }
         })
-      })
+    })
 }
-function getDragAfterElement(container, y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+function getDragAfterElement(target, x) {
+    const draggableElements = [...target.querySelectorAll('.draggable:not(.dragging)')]
   
     return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = y - box.top - box.height / 2
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child }
-      } else {
-        return closest
-      }
+        const box = child.getBoundingClientRect()
+        const offset = x - box.right - box.width / 2
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
     }, { offset: Number.NEGATIVE_INFINITY }).element
-  }
+}
 
 //Fisher-Yates Shuffle
 function shuffle(array) {   
@@ -99,8 +99,7 @@ randomizeBtn.addEventListener('click', () => {
         span.dataset.lastName = student.lastName
         span.textContent = `${student.firstName} ${student.lastName}`
         span.draggable = true
-        span.classList.add('draggable')
-        span.classList.add('rounded')
+        span.classList.add(['draggable', 'rounded'])
         return span
     })
 
